434b7e0038e8beca12486f7afc5629f64f8ebf9a
Hack Reactor Students <gregorylull@gmail.com>
2014-03-20 17:13:18 -0700

    Scaffolded Archive Helpers in checkArchive

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -23,21 +23,50 @@ exports.initialize = function(pathsObj){
   });
 };
 
+// logic
+/*
+  1. if inUrlList === true
+       if archived === true -> serve page
+       else                 -> serve loading
+         * note * use redirect
+  2. if inUrlList === false
+       1. write to list
+       2. serve loading page
+*/
 
 
 exports.checkArchive = function(res, url, callback) {
   url = 'www.google.com'; // Test Data
+  // 1. if inUrlList === true
+  exports.isUrlInList(url, function s (url) {
+  //      if archived === true -> serve page
+    exports.isURLArchived(url, function s (url) {
+      console.log('is archived, serve page');
+  //      else                 -> serve loading
+    } , function f (url){
+      console.log('is not archived, serve loading');
+    } );
+  // 2. if inUrlList === false
+  }, function f (url) {
+  //      1. write to list
+    console.log('write to list');
+    exports.addUrlToList(url);
+
+  //      2. serve loading page
+    console.log('serve loading page');
+
+  });
 
   callback(res, url);
 };
 
-exports.readListOfUrls = function(callback, arg){
+exports.readListOfUrls = function(callback){
   fs.readFile(paths.list, function (err, list) {
-    if (err) throw err;
-    list = list.split('\n');
+    if (err) { throw err; }
+    console.log(list);
     // parse list and load in memory
 
-    callback.call(list, arg);
+    callback(list);
   });
 };
 
@@ -49,12 +78,26 @@ exports.downloadUrls = function(target, callback){
   callback();
 };
 
-exports.isURLArchived = function(archive, target, callback){
+exports.isURLArchived = function(target, successCb, failureCb){
+  // how to read a directory?
+  fs.readdir(paths.archivedSites, function (err, dirSiteNames){
+    if (err) { throw err; }
+    if (dirSiteNames.indexOf(target) > -1) {
+      successCb(target);
+    } else {
+      failureCb(target);
+    }
+  });
   return true;
 };
 
-exports.isUrlInList = function(list, target, callback){
-  // read file
-  // if google is in file
+exports.isUrlInList = function(target, successCb, failureCb){
+  exports.readListOfUrls(function (data, target) {
+    if (data.indexOf(target) > -1) {
+       successCb(target);
+    } else {
+      failureCb(target);
+    }
+  });
   return true;
 };
